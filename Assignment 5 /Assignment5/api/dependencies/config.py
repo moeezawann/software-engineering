8339b3473668d919c0d328587665afb617a74290
moeezawann <moeez.awan15@gmail.com>
2024-11-02 22:14:59 -0400

    Testing .gitignore to make sure it is hiding db password

--- Assignment 5 /Assignment5/api/dependencies/config.py
+++ Assignment 5 /Assignment5/api/dependencies/config.py
@@ -1,6 +1,7 @@
+import os 
 class conf:
     host = "localhost"
     database = "sandwich_maker_api"
     port = 3306
     user = "root"
-    password = "rootroot"
+    password = os.getenv("DB_PASSWORD", "")
